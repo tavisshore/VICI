6a68d79bbda76523d40eab92e980ac574bc9819f
tavisshore <tgshore97@outlook.com>
2025-05-14 09:42:15 +0100

    updated eval to run from state dict

--- eval.py
+++ eval.py
@@ -1,11 +1,12 @@
 import argparse
+import torch
 import lightning.pytorch as pl
 from src.models.vanilla import Vanilla
 from config.cfg import get_cfg_defaults
 
 args = argparse.ArgumentParser()
 args.add_argument('--debug', action='store_true')
-args.add_argument('--config', type=str, default='default.yaml')
+args.add_argument('--config', type=str, default='DINO-L.yaml')
 args = vars(args.parse_args())
 
 arglist = []
@@ -18,7 +19,8 @@ cfg.merge_from_file(f'{cfg.system.path}/config/{args["config"]}')
 cfg.merge_from_list(arglist)
 cfg.system.results_path = 'results'
 
-model = Vanilla.load_from_checkpoint('/scratch/projects/challenge/results/DINO-L+D.ckpt', cfg=cfg)#, strict=False)
+model = Vanilla(cfg) #.load_from_checkpoint('/scratch/projects/challenge/results/DINO-L+D.ckpt', cfg=cfg)#, strict=False)
+model.model.load_state_dict(torch.load('DINO-L.pth'), strict=True)
 model = model.eval()
 
 trainer = pl.Trainer(devices=1, default_root_dir=cfg.system.results_path)
